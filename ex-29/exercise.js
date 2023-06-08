6185e5e15911d0d4c4fcd9c32e2808998acc1b77
Andrea Izzo <aizzo450@gmail.com>
2023-06-08 12:52:24 +0200

    svolto esercizio 29 javascript

---
+++ ex-29/exercise.js
@@ -0,0 +1,13 @@
+class Person {
+  constructor(id, firstName, lastName, age) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+}
+
+const developer = new Person(1, 'Mario', 'Rossi', 25);
+const devJSON = JSON.stringify(developer);
+console.log(devJSON);
+// Print developer as json object
