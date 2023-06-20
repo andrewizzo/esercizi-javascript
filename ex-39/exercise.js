cd7930508e1605c90a9d89d1903870b51336c92c
Andrea Izzo <aizzo450@gmail.com>
2023-06-20 13:01:58 +0200

    svolto esercizio 39 javascript

---
+++ ex-39/exercise.js
@@ -0,0 +1,37 @@
+const isLogged = true;
+
+const checkNumber = (para) => {
+    return new Promise ((resolve,reject) => {
+        if(para){
+            setTimeout(() => {
+                const randomNum = Math.random();
+                resolve(randomNum);
+
+            },1000);
+        }else{
+            reject("something went wrong")
+        }
+    });
+};
+
+const printPerson = (num) => {
+    return new Promise((resolve,reject) => {
+        if (num > 0.5){
+            setTimeout(() => {
+                const obj = {name: "John", age: 24};
+                resolve(obj);
+            },1000);
+        }else{
+            reject(new Error("something went wrong again"));
+        }
+    })
+}
+
+checkNumber(isLogged)
+    .then((randomNum) => printPerson(randomNum))
+    .then((obj) => {
+        console.log(obj);
+    })
+    .catch((error) => {
+        console.error(error);
+    })
